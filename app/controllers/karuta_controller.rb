83db7565700c6ab298399a9feafd78468abd8712
himajin315 <himajin315@gmail.com>
2014-10-20 16:16:30 +0900

    UIの部分を移した

--- app/controllers/karuta_controller.rb
+++ app/controllers/karuta_controller.rb
@@ -1,4 +1,24 @@
 class KarutaController < ApplicationController
+  require 'rexml/document'
+  require 'open-uri'
+
   def index
+    @question_text = question_text
+    @answer_text = answer_text.to_s.split('　')
+  end
+
+  def api_response
+    key = ENV["ASAHI_KENSAKU_API"]
+    REXML::Document.new(open("http://54.92.123.84/search?q=Title:%E3%83%89%E3%83%A9%E3%81%88%E3%82%82%E3%82%93%20AND%20ReleaseDate:2010-01-10&ackey=#{key}"))
+  end
+
+  def question_text
+    doc = api_response
+    question = doc.elements['response/result/doc/Body']
+    end
+
+  def answer_text
+    doc = api_response
+    answer = doc.elements['response/result/doc[2]/Body']
   end
 end
