a99d436c1855553de59056c7446cc717cfe0fc44
nanophate <z.nanophate@icloud.com>
2014-10-22 18:51:14 +0900

    added some registrationauth

---
+++ app/controllers/some_dir/registrations_controller.rb
@@ -0,0 +1,21 @@
+class SomeDir::RegistrationsController < Devise::RegistrationsController
+  def create
+    super
+    session[:omniauth] = nil uless @user.new_record?
+    # これはユーザーの登録に成功していればsession[:omniauth}は不要なので削除するという記述。
+  end
+
+  protected
+    #
+    # このメソッドを次のようにオーバーライドすることで
+    # 保存される前の @user に authentication を持たせることができる。
+    # @user が保存されれば authentication も保存される。
+    #
+    def build_resource(*args)
+      super
+       if session[:omniauth]
+          omniauth = session[:omniauth]
+          @user.authentications.build provider: omniauth[:provider], uid: omniauth[:uid]
+       end
+    end
+end
