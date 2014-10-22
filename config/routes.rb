6d27e6ea0b6b6cd3ef25f1305e528474a37bf403
nanophate <z.nanophate@icloud.com>
2014-10-22 22:31:41 +0900

    add omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -15,10 +16,10 @@ Rails.application.routes.draw do
      end
 
   #callback routing (remove this when doing [ $ rails g model..])
-  cdevise_for :users, controllers: {
-    omniauth_callbacks: "some_dir/omniauth_callbacks",
-    rgistrations:       "some_dir/registrations"
-   }
+  #devise_for :users, controllers: {
+  #  omniauth_callbacks: "some_dir/omniauth_callbacks",
+  #  rgistrations:       "some_dir/registrations"
+  # }
 
   # Examprele of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
