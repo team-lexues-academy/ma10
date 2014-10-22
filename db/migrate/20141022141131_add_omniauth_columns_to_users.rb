d8b82c73b31bb6437556ddf61891da32a55e6633
nanophate <z.nanophate@icloud.com>
2014-10-22 23:13:08 +0900

    index add

--- db/migrate/20141022141131_add_omniauth_columns_to_users.rb
+++ db/migrate/20141022141131_add_omniauth_columns_to_users.rb
@@ -1,7 +1,10 @@
 class AddOmniauthColumnsToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :uid, :string
-    add_column :users, :provider, :string
+    add_column :users, :uid, :string, null: false, default: ""
+    add_column :users, :provider, :string, null: false, default: ""
     add_column :users, :name, :string
+
+    add_index :users, [:uid, :provider], unique: true
+
   end
 end
