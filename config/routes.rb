65a04e012d107a145682ef1e512506a2f5111df2
nanophate <z.nanophate@icloud.com>
2014-10-22 21:48:16 +0900

    added routes config for sign in and out

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,13 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  
+  # Devise sign in and signout
+  devise_scope :user do
+  	get 'sign_in',  to: 'users/sessions#new',     as: :new_user_session
+  	get 'sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
+     end
+  
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
