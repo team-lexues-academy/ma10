b32e27db6eec5e7bf58dc8560f54295744e5ea67
nanophate <z.nanophate@icloud.com>
2014-10-22 20:55:43 +0900

    Revert "@enokawa Merge pull request #1 from team-lexues-academy/refactoring-api-request"
    This reverts commit 6e179625928010cd60e37abad7c87218081c0e95.

--- app/controllers/karuta_controller.rb
+++ app/controllers/karuta_controller.rb
@@ -1,12 +1,24 @@
 class KarutaController < ApplicationController
+  require 'rexml/document'
+  require 'open-uri'
+
   def index
-    doc = DoraemonQuestion.new
+    @question_text = question_text
+    @answer_text = answer_text.to_s.split('　')
+  end
 
-    # 質問を取得
-    @question = doc.question
+  def api_response
+    key = ENV["ASAHI_KENSAKU_API"]
+    REXML::Document.new(open("http://54.92.123.84/search?q=Title:%E3%83%89%E3%83%A9%E3%81%88%E3%82%82%E3%82%93%20AND%20ReleaseDate:2010-01-10&ackey=#{key}"))
+  end
 
-    # 答えを取得
-    @answer = doc.answer
-    @answer_detail = doc.answer_detail
+  def question_text
+    doc = api_response
+    question = doc.elements['response/result/doc/Body']
+    end
+
+  def answer_text
+    doc = api_response
+    answer = doc.elements['response/result/doc[2]/Body']
   end
 end
