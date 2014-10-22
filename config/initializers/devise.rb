6ed7df25e309a7596297f3cc5c9bbbd67f40237b
nanophate <z.nanophate@icloud.com>
2014-10-22 18:01:21 +0900

    added route and initializer

---
+++ config/initializers/devise.rb
@@ -0,0 +1,1 @@
+config.omniauth :twitter, ENV['TWITTER_KEY'],ENV['TWITTER_SECRET']
--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  devise_scope :user do
+     get 'sign_in',  to: 'users/sessions#new',	   as: :new_user_session
+     get 'sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
 end
