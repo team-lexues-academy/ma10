984ec52486e035e02676132cabf8b278f8e22f41
nanophate <z.nanophate@icloud.com>
2014-10-22 23:23:54 +0900

    del unneeded

--- test/controllers/users/passwords_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Users::PasswordsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/helpers/users/passwords_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class Users::PasswordsHelperTest < ActionView::TestCase
-end
