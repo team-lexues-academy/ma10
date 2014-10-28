9ce7147f6755ca9d4ce35b61382e354165bbf358
enokawa <enokawa00@gmail.com>
2014-10-29 01:22:50 +0900

    result["response"]["result"]["doc"]がnilだったらもう一度APIを叩く

--- app/models/api_request.rb
+++ app/models/api_request.rb
@@ -14,14 +14,14 @@ class ApiRequest
   def response
     key = ENV["ASAHI_KENSAKU_API"]
     title = "Title:%E3%83%89%E3%83%A9%E3%81%88%E3%82%82%E3%82%93" #ドラえもん
-    release_date = "ReleaseDate:#{rand_date("2010-10-06","2011-10-10")}"
-    format = "wt=json"
-
     begin
-    url = URI.parse("http://54.92.123.84/search?q=#{title}%20AND%20#{release_date}&#{format}&ackey=#{key}")
-    json = Net::HTTP.get(url)
-    result = JSON.parse(json)
-    end while result["response"]["result"]["numFound"] == 0
+      release_date = "ReleaseDate:#{rand_date("2010-10-06","2011-10-10")}"
+      format = "wt=json"
+      body = "Body:Fdujiko-Pro"
+      url = URI.parse("http://54.92.123.84/search?q=#{title}%20AND%20#{body}%20AND%20#{release_date}&#{format}&ackey=#{key}")
+      json = Net::HTTP.get(url)
+      result = JSON.parse(json)
+    end while result["response"]["result"]["doc"].nil?
     return result
   end
 end
