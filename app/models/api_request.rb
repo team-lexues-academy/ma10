030f94476f2d03b0b3fd8d535ac3a168f8b1c4e2
enokawa <enokawa00@gmail.com>
2014-10-28 15:22:32 +0900

    Change json

--- app/models/api_request.rb
+++ app/models/api_request.rb
@@ -7,7 +7,9 @@ class ApiRequest
     key = ENV["ASAHI_KENSAKU_API"]
     title = "Title:%E3%83%89%E3%83%A9%E3%81%88%E3%82%82%E3%82%93" #ドラえもん
     release_date = "ReleaseDate:2010-01-10"
-    url = "http://54.92.123.84/search?q=#{title}%20AND%20#{release_date}&ackey=#{key}"
-    REXML::Document.new(open(url))
+    format = "wt=json"
+    url = URI.parse("http://54.92.123.84/search?q=#{title}%20AND%20#{release_date}&#{format}&ackey=#{key}")
+    json = Net::HTTP.get(url)
+    result = JSON.parse(json)
   end
 end
