25b020a64b767368df7fe5ad07df62e03d853c5a
enokawa <enokawa00@gmail.com>
2014-10-28 18:37:14 +0900

    ランダムで問題と答えを表示できるようにした

--- app/models/api_request.rb
+++ app/models/api_request.rb
@@ -2,11 +2,19 @@ class ApiRequest
   require 'net/http'
   require 'uri'
   require 'json'
+  require 'date'
+
+  def rand_date(d_from,d_to)
+    from = Date.parse(d_from)
+    to = Date.parse(d_to)
+    days = to - from + 1
+    return from + rand(days)
+  end
 
   def response
     key = ENV["ASAHI_KENSAKU_API"]
     title = "Title:%E3%83%89%E3%83%A9%E3%81%88%E3%82%82%E3%82%93" #ドラえもん
-    release_date = "ReleaseDate:2010-01-10"
+    release_date = "ReleaseDate:#{rand_date("2010-10-06","2011-10-10")}"
     format = "wt=json"
     url = URI.parse("http://54.92.123.84/search?q=#{title}%20AND%20#{release_date}&#{format}&ackey=#{key}")
     json = Net::HTTP.get(url)
