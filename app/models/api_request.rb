e786e9636264b971fcb8a752179a97f16e68198a
enokawa <enokawa00@gmail.com>
2014-10-28 22:22:17 +0900

    numFoundが0じゃない時だけ質問と答えを表示したい

--- app/models/api_request.rb
+++ app/models/api_request.rb
@@ -16,8 +16,12 @@ class ApiRequest
     title = "Title:%E3%83%89%E3%83%A9%E3%81%88%E3%82%82%E3%82%93" #ドラえもん
     release_date = "ReleaseDate:#{rand_date("2010-10-06","2011-10-10")}"
     format = "wt=json"
+
+    begin
     url = URI.parse("http://54.92.123.84/search?q=#{title}%20AND%20#{release_date}&#{format}&ackey=#{key}")
     json = Net::HTTP.get(url)
     result = JSON.parse(json)
+    end while result["response"]["result"]["numFound"] == 0
+    return result
   end
 end
