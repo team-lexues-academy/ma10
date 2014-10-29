015a02f983907fea282610694d3178edb6258b6a
himajin315 <himajin315@gmail.com>
2014-10-29 11:43:54 +0900

    Refactoring api_request.rb, doraemon_question.rb

--- app/models/api_request.rb
+++ app/models/api_request.rb
@@ -4,24 +4,11 @@ class ApiRequest
   require 'json'
   require 'date'
 
-  def rand_date(d_from,d_to)
-    from = Date.parse(d_from)
-    to = Date.parse(d_to)
-    days = to - from + 1
-    return from + rand(days)
-  end
-
-  def response
+  def request(search_word)
     key = ENV["ASAHI_KENSAKU_API"]
-    title = "Title:%E3%83%89%E3%83%A9%E3%81%88%E3%82%82%E3%82%93" #ドラえもん
-    begin
-      release_date = "ReleaseDate:#{rand_date("2010-10-06","2011-10-10")}"
-      format = "wt=json"
-      body = "Body:Fdujiko-Pro"
-      url = URI.parse("http://54.92.123.84/search?q=#{title}%20AND%20#{body}%20AND%20#{release_date}&#{format}&ackey=#{key}")
-      json = Net::HTTP.get(url)
-      result = JSON.parse(json)
-    end while result["response"]["result"]["doc"].nil?
-    return result
+    format = "wt=json"
+    url = URI.parse("http://54.92.123.84/search?q=#{search_word}&#{format}&ackey=#{key}")
+    json = Net::HTTP.get(url)
+    JSON.parse(json)
   end
 end
