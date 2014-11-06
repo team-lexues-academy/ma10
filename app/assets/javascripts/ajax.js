3cde8cb78eb6b207457b0d935c82984bf3337768
enokawa <enokawa00@gmail.com>
2014-11-06 16:59:29 +0900

    クリックをカウントしたい

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -1,12 +1,12 @@
-var cnt = 0;
+var cnt = 0; //cntを初期化
 
 $(function() {
   $('.karuta').click(function() {
     $('#question-karuta').load('karuta.html');
-    if(cnt == 6){
-      alert('終わりです');
+    if(cnt == 6){ //もしcntが6だったら
+      alert('終わりです'); //ここでアラートを出したい
     }else{
-      cnt = cnt + 1;
+      cnt = cnt + 1;  //cntが6でなければインクリメント
     }
   });
 });
