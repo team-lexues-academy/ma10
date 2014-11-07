847a66067d5e970c11795d9e416d641d4e2aba13
enokawa <enokawa00@gmail.com>
2014-11-08 07:11:07 +0900

    level2もjsonで表示するように修正。

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -13,7 +13,9 @@ $(function() {
 });
 
 var result ='';
+var result2 ='';
 var question='';
+
 $.post("karuta/doraemon.json",
   function(data){
     result += '<table>';
@@ -23,16 +25,48 @@ $.post("karuta/doraemon.json",
     result += '<div class="karutaData">' + data["0"][2] + '</div>';
     result += '</td>';
     result += '<td>';
-    result += '<div class="karuta">甘酸っぱい<%= image_tag("flip.png") %></div>';
-    result += '<div class="karutaData">え？<img src="wrong.png"></div>';
+    result += '<div class="karuta">甘酸っぱい</div>';
+    result += '<div class="karutaData">え？</div>';
     result += '</td>';
     result += '</tr>';
     result += '</table>';
     question += data["0"][0];
 
+    result2 += '<table>';
+    result2 += '<tr>';
+    result2 += '<td>';
+    result2 += '<div class="karuta">' + data["0"][1] + '</div>';
+    result2 += '<div class="karutaData">' + data["0"][2] + '</div>';
+    result2 += '</td>';
+    result2 += '<td>';
+    result2 += '<div class="karuta">甘酸っぱい</div>';
+    result2 += '<div class="karutaData">え？</div>';
+    result2 += '</td>';
+    result2 += '<td>';
+    result2 += '<div class="karuta">おおむね赤</div>';
+    result2 += '<div class="karutaData">え？</div>';
+    result2 += '</td>';
+    result2 += '</tr>';
+    result2 += '<tr>';
+    result2 += '<td>';
+    result2 += '<div class="karuta">' + data["0"][1] + '</div>';
+    result2 += '<div class="karutaData">' + data["0"][2] + '</div>';
+    result2 += '</td>';
+    result2 += '<td>';
+    result2 += '<div class="karuta">甘酸っぱい</div>';
+    result2 += '<div class="karutaData">え？</div>';
+    result2 += '</td>';
+    result2 += '<td>';
+    result2 += '<div class="karuta">おおむね赤</div>';
+    result2 += '<div class="karutaData">え？</div>';
+    result2 += '</td>';
+    result2 += '</tr>';
+    result2 += '</table>';
+
     console.log(data);
     console.log(result);
     $("#result").append(result);
+    $("#result2").append(result2);
     $("#quetioin-area").append(question);
 
   }
