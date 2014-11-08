120fa9ea61e0630fca592b6c8e43b331b4c51ac7
himajin315 <himajin315@gmail.com>
2014-11-08 09:49:36 +0900

    問題が切り替わるように変更 ６問正解したら、終了するようにした

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -1,36 +1,29 @@
-var cnt = 0; //cntを初期化
-
-
 $(function() {
-  $('.karuta').click(function() {
-    $('#question-karuta').load('karuta.html');
-    if(cnt == 6){ //もしcntが6だったら
-      alert('終わりです'); //ここでアラートを出したい
-    }else{
-      cnt = cnt + 1;  //cntが6でなければインクリメント
+  var cnt = 0; //cntを初期化
+  var answer = Math.round( Math.random()*1 );
+  var answer_tag = '#answer' + answer;
+
+
+  $.post("karuta/doraemon.json",apiCallback);
+
+  function apiCallback(data) {
+    var result ='';
+    var result2 ='';
+    var question='';
+    var question_number = 2;
+    var number = Math.round( Math.random()*8 );
+    var next_number = number + 1;
+
+    for(i=0; i<2; i++ ){
+      result += '<td>';
+      if (answer == i ) {
+        $(answer_tag).append(data["0"][1]);
+      } else {
+        var other_answer_tag = '#answer' + i;
+        $(other_answer_tag).append(data["1"][1]);
+      }
     }
-  });
-});
-
-var result ='';
-var result2 ='';
-var question='';
-
-$.post("karuta/doraemon.json",
-  function(data){
-    result += '<table>';
-    result += '<tr>';
-    result += '<td>';
-    result += '<div class="karuta">' + data["0"][1] + '</div>';
-    result += '<div class="karutaData">' + data["0"][2] + '</div>';
-    result += '</td>';
-    result += '<td>';
-    result += '<div class="karuta">甘酸っぱい</div>';
-    result += '<div class="karutaData">え？</div>';
-    result += '</td>';
-    result += '</tr>';
-    result += '</table>';
-    question += data["0"][0];
+    question += data[number][0];
 
     result2 += '<table>';
     result2 += '<tr>';
@@ -63,11 +56,22 @@ $.post("karuta/doraemon.json",
     result2 += '</tr>';
     result2 += '</table>';
 
-    console.log(data);
-    console.log(result);
-    $("#result").append(result);
     $("#result2").append(result2);
     $("#quetioin-area").append(question);
-
   }
-);
+
+  $(answer_tag).click(function() {
+      alert('正解');
+      $("#quetioin-area").empty();
+      $("#answer0").empty();
+      $("#answer1").empty();
+      $.post("karuta/doraemon.json",apiCallback);
+      cnt = cnt + 1;  //cntが6でなければインクリメント
+  });
+
+  $('.karuta').click(function() {
+    if(cnt == 6){ //もしcntが6だったら
+      $(location).attr('href', 'http://www.city.tamba.hyogo.jp/site/chi-tan/');
+    }
+  });
+});
